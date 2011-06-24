0b875cf23fbb5d8e77a6a62f191737f024f44277
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2011-06-24 11:32:31 +0200

    Added capybara and a working js driver.

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -6,6 +6,10 @@ Spork.prefork do
   require 'rspec/rails'
   require 'nulldb_rspec'
 
+  require 'capybara/rspec'
+  require 'akephalos'
+  Capybara.javascript_driver = :akephalos
+
   Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
   RSpec.configure do |config|
@@ -14,7 +18,7 @@ Spork.prefork do
 
     if ENV['FAST_TESTS']
       config.filter_run_excluding :db => true
-      config.filter_run_excluding :request => true
+      config.filter_run_excluding :type => :request
     end
 
     config.before(:all, :db => true) do 
@@ -24,10 +28,17 @@ Spork.prefork do
     config.after(:all, :db => true) do
       ActiveRecord::Base.establish_connection :adapter => :nulldb
     end
+
+    config.before(:all, :type => :request) do 
+      ActiveRecord::Base.establish_connection :test
+    end
+
+    config.after(:all, :type => :request) do
+      ActiveRecord::Base.establish_connection :adapter => :nulldb
+    end
   end
 end
 
 Spork.each_run do
   ActiveRecord::Base.establish_connection :adapter => :nulldb
 end
-
